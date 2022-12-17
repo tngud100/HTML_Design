d884214b658cf37ab0bc3fb96a4a92bdd919dbb6
33285109@naver.com <33285109@naver.com>
2022-12-17 14:04:41 +0900

    product_js

--- css/js/product_enroll.js
+++ css/js/product_enroll.js
@@ -1,15 +1,41 @@
-$(function(){
-    $('#enroll_product').click(function(){
-        $('.back_ignore').show()
-        $('#enroll_form').animate({
-            top: '0'
-        },1000);
-    });
+$(function () {
+  $("#enroll_product").click(function () {
+    $(".back_ignore").show();
+    $("#enroll_form").animate(
+      {
+        top: "0",
+      },
+      1000
+    );
+  });
 
-    $('.btn-close').click(function(){
-        $('.back_ignore').hide()
-        $('#enroll_form').animate({
-            top:'-2000px'
-        },1000);
-    });
+  $(".btn-close").click(function () {
+    $(".back_ignore").hide();
+    $("#enroll_form").animate(
+      {
+        top: "-2000px",
+      },
+      1000
+    );
+  });
+
+  set_change_img();
 });
+
+const set_change_img = () => {
+  const product_image = document.getElementById("product_img");
+  const input_image = document.getElementById("inputGroupFile02");
+
+  input_image.addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+      const myFile = this.files[0];
+      const reader = new FileReader();
+
+      reader.addEventListener("load", function (e) {
+        product_image.setAttribute("src", e.target.result);
+      });
+
+      reader.readAsDataURL(myFile);
+    }
+  });
+};
